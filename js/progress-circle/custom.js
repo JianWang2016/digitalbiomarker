86146a7ea6988b89bcab9159f4d89054d34daf75
Jian Wang <jianwang2010@gmail.com>
2017-07-17 15:28:26 -0400

    Initial commit

---
+++ js/progress-circle/custom.js
@@ -0,0 +1,42 @@
+	(function($) {
+		"use strict";
+	
+	
+
+	$(function () {
+	$('.percent').percentageLoader({
+		valElement: 'p',
+		strokeWidth:5,
+		bgColor: 'rgba(255, 255, 255, 0.1)',
+		ringColor: '#2948e3',
+		textColor: '#fff',
+		fontSize: '24px',
+		fontWeight: 'normal'
+	});
+
+	});
+
+
+	$(function () {
+	$('.percent-2').percentageLoader({
+		valElement: 'p',
+		strokeWidth:5,
+		bgColor: 'rgba(255, 255, 255, 0.1)',
+		ringColor: '#2948e3',
+		textColor: '#fff',
+		fontSize: '24px',
+		fontWeight: 'normal'
+	});
+
+	});
+
+
+
+
+
+
+
+	
+
+	
+	})(jQuery);
